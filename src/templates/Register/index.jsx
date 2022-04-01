d1f3f9855e902d6338b714ec0e0455e9df89172e
Wendel Demétrio <hey.wendel95@gmail.com>
2022-04-01 16:20:28 -0300

    useAxios handling errors and loading

--- src/templates/Register/index.jsx
+++ src/templates/Register/index.jsx
@@ -1,39 +1,32 @@
 import { useAxios } from '../../hooks/useAxios';
-import axios from 'axios';
-import { useEffect } from 'react';
 
 const Register = () => {
-  const fetchData = async () => {
-    axios
-      .post(
-        'https://can-eat-api.herokuapp.com/auth/register',
-      )
-      .then((res) => console.log(res.data))
-      .catch((err) => console.log(JSON.stringify(err)));
-  };
+  const { response, loading, error } = useAxios({
+    method: 'POST',
+    url: '/auth/register',
+    headers: {
+      accept: '*/*',
+    },
+    data: {
+      name: 'Wendel Demétrio',
+      email: 'teste11234ddsa3',
+      password: 'seila',
+      confirmpassword: 'seila',
+    },
+  });
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-  // const { response, loading, error } = useAxios({
-  //   method: 'POST',
-  //   url: '/auth/register',
-  //   headers: {
-  //     accept: '*/*',
-  //   },
-  //   data: {
-  //     name: 'Wendel Demétrio',
-  //     email: 'teste1',
-  //     password: 'seila',
-  //     confirmpassword: 'seila',
-  //   },
-  // });
+  fetch().then(res => { array.push(res))}
+
+  if (error) {
+    return <h1>{error.response.data.msg}</h1>;
+  }
+  if (loading) {
+    return <h1>Carregando..</h1>;
+  }
   return (
-    <>
-      <div>
-        <h1>Register</h1>
-      </div>
-    </>
+    <div>
+      <h1>{response.msg}</h1>
+    </div>
   );
 };
 
