1e20e5f417486323a100bfc9552682bd75f52175
Wendel (anti-w) Demétrio <hey.wendel95@gmail.com>
2023-06-05 06:49:40 -0300

    pointing for new api on railway

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://can-eat-api.herokuapp.com',
+  baseURL: 'https://can-eat-api-production.up.railway.app/',
 });
 
 export default api;
--- src/services/foods.js
+++ src/services/foods.js
@@ -1,9 +1,11 @@
 import api from './api';
 
 export const getFoodsPerPages = (page) => {
-  return api.get(`/foods/${page}`);
+  return api.get(`/foods?page=${page}`);
 };
 
 export const getFoodsByGroup = (selectedGroup, page) => {
-  return api.get(`/foods/${selectedGroup}/${page}`);
+  return api.get(
+    `/foods?group=${selectedGroup}&page=${page}`,
+  );
 };
